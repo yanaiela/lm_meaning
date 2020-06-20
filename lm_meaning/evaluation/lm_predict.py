d4a4bf4ec65d7783dc8e74e266eebc81b899f830
Yanai Elazar <yanaiela@gmail.com>
2020-06-20 13:31:16 +0300

    normalizing the tokens intro regular strings

--- lm_meaning/evaluation/lm_predict.py
+++ lm_meaning/evaluation/lm_predict.py
@@ -27,21 +27,20 @@ def get_predictions(tokenizer, lm_model, tokenized_sentences, target_idx, k=10):
     Assuming sentences are of the same length
     :param tokenizer: model tokenizer
     :param lm_model: lm model
-    :param sentences: a btach of sentences with the [MASK] argument
+    :param tokenized_sentences: a batch of sentences with the [MASK] argument
     :param k: top k most probable words
     :return: list of lists, corresponding to the sentence batch and top k words in each one
     """
-    # tokenized_sentences, target_idx = sentences2ids(sentences, tokenizer)
     prediction_scores = lm_model(torch.tensor(tokenized_sentences))[0]
-    # token_scores = prediction_scores[:, target_idx].detach().cpu().numpy()
     token_scores = torch.stack([x[y] for x, y in zip(prediction_scores, target_idx)]).detach().cpu().numpy()
     best_k = (np.argsort(token_scores, axis=1))[:, -k:]
     sentences_best_k = []
     for top_per_sentence in best_k:
         best_k_tokens = tokenizer.convert_ids_to_tokens(top_per_sentence)
 
-        # getting rid of roberta's byte-level tokens
-        best_k_tokens = [x.replace('Ġ', '') for x in best_k_tokens]
+        # "normalizing" the tokens to 'standard' strings
+        best_k_tokens = [tokenizer.convert_tokens_to_string(x) for x in best_k_tokens]
+        # best_k_tokens = [x.replace('Ġ', '') for x in best_k_tokens]
 
         sentences_best_k.append(best_k_tokens[::-1])
     return sentences_best_k
