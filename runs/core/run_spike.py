b489a006494856fe0d9886e75d890da71e3011a0
Yanai Elazar <yanaiela@gmail.com>
2020-09-10 14:57:00 +0300

    run spike update

--- runs/core/run_spike.py
+++ runs/core/run_spike.py
@@ -15,27 +15,24 @@ nodes = [
 ]
 
 
-# ┌──────────────────────┐
-# │ generate experiments │
-# └──────────────────────┘
-
-relations = [
-    'P19',
-    'P449',
-]
-
 if __name__ == '__main__':
 
     parse = argparse.ArgumentParser("")
     parse.add_argument("-dry_run", "--dry_run", type=bool, help="flag to only print commands and not execute them",
                        default=False)
+    parse.add_argument("-patterns", "--patterns", type=str, help="patterns file",
+                       default="runs/core/patterns.txt")
     args = parse.parse_args()
+
+    with open(args.patterns, 'r') as f:
+        relations = f.readlines()
+        relations = [x.strip() for x in relations]
 
     cartesian_product = []
     for relation_id in relations:
-        cartesian_product.append([f'data/TREx_train/{relation_id}.jsonl',
+        cartesian_product.append([f'data/trex/data/TREx/{relation_id}.jsonl',
                                   f'data/pattern_data/{relation_id}.jsonl',
-                                  f'data/output/spike_results/TREx_train/{relation_id}.json'
+                                  f'data/output/spike_results/{relation_id}.json'
                                   ])
 
     parallelize(nodes, cartesian_product, '/home/nlp/lazary/workspace/thesis/lm_meaning/runs/core/run_spike.sh',
