1a116a94591f9f46aab984876badbadf39214aee
Yanai Elazar <yanaiela@gmail.com>
2020-09-17 12:11:52 +0300

    cleaning the repo from old code (which was moved to a new branch)

--- lm_meaning/utils.py
+++ lm_meaning/utils.py
@@ -1,31 +1,5 @@
 import json
-import string
 from typing import List, Dict
-
-from transformers import *
-
-model_name = 'roberta-large'
-
-model = RobertaForMaskedLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model.eval()
-
-
-def filter_vocab(items):
-    filtered_items = []
-
-    for item in items:
-        tok_v = tokenizer.tokenize(item)
-
-        if len(tok_v) != 1:
-            continue
-
-        if not all([x in (string.ascii_lowercase + string.ascii_uppercase) for x in tok_v[0]]):
-            continue
-
-        filtered_items.append(item[0])
-
-    return filtered_items
 
 
 def read_data(filename: str) -> List[Dict]:
@@ -37,18 +11,6 @@ def read_data(filename: str) -> List[Dict]:
             dataset.append(loaded_example)
 
     return dataset
-
-
-def filter_data_fields(data):
-    return [{"sub_label": sample["sub_label"], "obj_label":sample["obj_label"]} for sample in data]
-
-
-def parse_prompt(prompt: str, subject_label: str, object_label: str) -> str:
-    SUBJ_SYMBOL = '[X]'
-    OBJ_SYMBOL = '[Y]'
-    prompt = prompt.replace(SUBJ_SYMBOL, subject_label)
-    prompt = prompt.replace(OBJ_SYMBOL, object_label)
-    return prompt
 
 
 def load_prompts(filename: str) -> List[str]:
