256d8de1013d6e08a116da5cb48ab0f4bbe12623
Yanai Elazar <yanaiela@gmail.com>
2020-09-23 16:18:06 +0300

    to json script improvement

--- lm_meaning/spike_patterns/to_json.py
+++ lm_meaning/spike_patterns/to_json.py
@@ -1,39 +1,51 @@
+import argparse
 import json
-import sys
 
 
-fname = sys.argv[1]
-subj, obj = sys.argv[2], sys.argv[3]
+def main():
+    parse = argparse.ArgumentParser("")
+    parse.add_argument("-patterns_file", "--patterns_file", type=str, help=" path to the pattern file",
+                       default="data/pattern_data/P449.tsv")
+    parse.add_argument("-subject", "--subject", type=str, help="name of the subject",
+                       default="Friends")
+    parse.add_argument("-object", "--object", type=str, help="name of the object",
+                       default="NBC")
+    args = parse.parse_args()
 
-with open(fname, "r") as f:
-    lines = f.readlines()
-    
+    fname = args.patterns_file
+    subj, obj = args.subject, args.object
 
-print(fname)
-with open(fname.replace(".tsv","")+".jsonl", "w") as f:
-    for line in lines[1:]:
+    with open(fname, "r") as f:
+        lines = f.readlines()
 
-       vals = line.strip().split("\t")
+    print(fname)
+    with open(fname.replace(".tsv", "") + ".jsonl", "w") as f:
+        for line in lines[1:]:
 
-       pattern = vals[0]# vals[0].replace("X","[X]").replace("Y", "[Y]")
-       example = vals[1]
+            vals = line.strip().split("\t")
 
-       lemma, extended_lemma, tense = vals[2:]
-       #vals = vals[:-1] + vals[-1].split(",")
-       
-       # add spike syntax
-       spike_query = pattern.replace("[X]", "<>subject:"+subj).replace("[Y]", "object:[w={}]"+obj)
-       spike_query = spike_query.split(" ")
-       
-       for i,w in enumerate(spike_query):
-        if "subject" not in w and "object" not in w:
-            spike_query[i] = "$" + w
-       spike_query =  " ".join(spike_query)
-       
-       # save as json
-       
-       vals = [pattern, example, lemma, extended_lemma, tense, spike_query]
-       keys = ["pattern", "example", "lemma", "extended_lemma", "tense", "spike_query"]
-       dict = {k:v for k,v in zip(keys, vals)}
+            pattern = vals[0]
+            example = vals[1]
 
-       f.write(json.dumps(dict) + "\n")
+            lemma, extended_lemma, tense = vals[2:]
+            # vals = vals[:-1] + vals[-1].split(",")
+
+            # add spike syntax
+            spike_query = pattern.replace("[X]", "<>subject:" + subj).replace("[Y]", "object:[w={}]" + obj)
+            spike_query = spike_query.split(" ")
+
+            for i, w in enumerate(spike_query):
+                if "subject" not in w and "object" not in w:
+                    spike_query[i] = "$" + w
+            spike_query = " ".join(spike_query)
+
+            # save as json
+            vals = [pattern, example, lemma, extended_lemma, tense, spike_query]
+            keys = ["pattern", "example", "lemma", "extended_lemma", "tense", "spike_query"]
+            dict = {k: v for k, v in zip(keys, vals)}
+
+            f.write(json.dumps(dict) + "\n")
+
+
+if __name__ == '__main__':
+    main()
