2654e51cafff364d0ce36d69ba972493c49601a7
Yanai Elazar <yanaiela@gmail.com>
2020-09-24 16:23:54 +0300

    remove older bug handling where the iterator stoped. added new exception catching when disconnecting from frigg

--- lm_meaning/spike/run_syntactic_queries.py
+++ lm_meaning/spike/run_syntactic_queries.py
@@ -1,17 +1,18 @@
 import argparse
 import signal
 from collections import defaultdict
+from typing import List, Iterator, Optional
 
-from tqdm import tqdm
-from typing import List, Iterator
-
-from lm_meaning.spike.utils import get_spike_objects, get_relations_data, dump_json
+import wandb
+from requests.exceptions import RequestException
 from spike.annotators.annotator_service import Annotator
+from spike.integration.odinson.common import OdinsonContinuationToken
 from spike.search.engine import MatchEngine
 from spike.search.queries.common.match import SearchMatch
 from spike.search.queries.q import StructuredSearchQuery
+from tqdm import tqdm
 
-import wandb
+from lm_meaning.spike.utils import get_spike_objects, get_relations_data, dump_json
 
 
 def log_wandb(args):
@@ -29,27 +30,15 @@ def log_wandb(args):
     )
 
 
-def construct_query(engine: MatchEngine, annotator: Annotator, objs: List[str], query_str: str
-                    ) -> Iterator[SearchMatch]:
+def construct_query(engine: MatchEngine, annotator: Annotator, objs: List[str], query_str: str,
+                    continuation_token: Optional[OdinsonContinuationToken] = None) -> Iterator[SearchMatch]:
     filt_objs = ['`' + x + '`' for x in objs]
 
     query_with_objs = query_str.format('|'.join(filt_objs))
 
     search_query = StructuredSearchQuery(query_with_objs, annotator=annotator)
-    query_match = engine.match(search_query)
+    query_match = engine.match(search_query, continuation=continuation_token)
     return query_match
-
-
-class TimeoutException(Exception):  # custom exception
-    pass
-
-
-def timeout_handler(signum, frame):  # raises exception when signal sent
-    raise TimeoutException
-
-
-# Makes it so that when SIGALRM signal sent, it calls the function timeout_handler, which raises your exception
-signal.signal(signal.SIGALRM, timeout_handler)
 
 
 def main():
@@ -79,37 +68,36 @@ def main():
     for pattern in tqdm(patterns):
         query_match = construct_query(spike_engine, spike_annotator, list(obj_dic.keys()), pattern)
 
-        pbar = tqdm()
-        while True:
-            signal.alarm(15)
+        more_results = True
+        continuation_token = None
+        while more_results:
             try:
-                r = next(query_match, None)
-                if r is None:
-                    break
-                obj = r.sentence.words[r.captures['object'].first]
-                # to capture multi words expressions
-                subj = ' '.join(r.sentence.words[r.captures['subject'].first: r.captures['subject'].last + 1])
-                if obj in obj_dic:
-                    if subj in obj_dic[obj]:
-                        sentence = r.sentence.words
-                        data_dic[obj][subj][pattern] = sentence
-                pbar.update(1)
-            except TimeoutException:
-                print('time out exception')
-                break
-        # for r in tqdm(query_match):
-        #     obj = r.sentence.words[r.captures['object'].first]
-        #     subj = r.sentence.words[r.captures['subject'].first]
-        #     if obj in obj_dic:
-        #         if subj in obj_dic[obj]:
-        #             sentence = r.sentence.words
-        #             data_dic[obj][subj][pattern] = sentence
+                for r in tqdm(query_match):
+                    if r is None:
+                        break
+                    continuation_token = r.continuation_token
+                    obj = r.sentence.words[r.captures['object'].first]
+                    # to capture multi words expressions
+                    subj = ' '.join(r.sentence.words[r.captures['subject'].first: r.captures['subject'].last + 1])
+                    if obj in obj_dic:
+                        if subj in obj_dic[obj]:
+                            sentence = r.sentence.words
+                            data_dic[obj][subj][pattern] = sentence
+                more_results = False
+            except (ConnectionResetError, RequestException) as connection_error:
+                query_match = construct_query(spike_engine, spike_annotator, list(obj_dic.keys()), pattern,
+                                              continuation_token)
 
     c = 0
     for obj, v in data_dic.items():
         for subj, vv in v.items():
             c += len(vv)
     print('total number of objects found with all queries', c)
+    wandb.run.summary['n_matches'] = c
+    wandb.run.summary['n_patterns'] = len(patterns)
+    wandb.run.summary['n_relations'] = len(relations)
+    wandb.run.summary['n_subjects'] = len(list(set(obj_dic.values())))
+    wandb.run.summary['n_objects'] = len(list(set(obj_dic.keys())))
 
     dump_json(data_dic, args.spike_results)
 
