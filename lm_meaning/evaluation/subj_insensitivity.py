6c5aae8e6e09d89dd2473ed1d48730393d89eac9
Yanai Elazar <yanaiela@gmail.com>
2020-11-03 15:00:33 +0200

    insensitivity updates

--- lm_meaning/evaluation/subj_insensitivity.py
+++ lm_meaning/evaluation/subj_insensitivity.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 from collections import defaultdict
+from scipy.stats import entropy
+from glob import glob
 
 
 def get_obj2subj(relation_predictions):
@@ -8,9 +10,12 @@ def get_obj2subj(relation_predictions):
     preds = relation_predictions['predictions']
 
     predictions_dic = defaultdict(list)
+    predictions, truth = [], []
     for data_row, pred_row in zip(data, preds):
         predictions_dic[pred_row[0]['token_str']].append(data_row['obj_label'])
-    return predictions_dic
+        predictions.append(pred_row[0]['token_str'])
+        truth.append(data_row['obj_label'])
+    return predictions, truth
 
 
 def main():
@@ -20,26 +25,23 @@ def main():
 
     args = parse.parse_args()
 
-    with open(args.lm_preds, 'r') as f:
-        lm_preds = json.load(f)
+    for f_name in glob(args.lm_preds + '/*'):
+        pattern_id = f_name.split('/')[-1].split('_')[0]
+        with open(f_name, 'r') as f:
+            lm_preds = json.load(f)
 
-    orig_pattern = list(lm_preds.keys())[0]
-    relation_predictions = lm_preds[orig_pattern]
+        orig_pattern = list(lm_preds.keys())[0]
+        relation_predictions = lm_preds[orig_pattern]
 
-    obj2subjs = get_obj2subj(relation_predictions)
+        predictions, truths = get_obj2subj(relation_predictions)
 
-    max_num = -1
-    largest_pred_list = []
-    most_predicted_obj = None
-    for pred_obj, vals in obj2subjs.items():
-        print(pred_obj, vals.count(pred_obj), len(vals))
-        if len(vals) > max_num:
-            largest_pred_list = vals
-            most_predicted_obj = pred_obj
-            max_num = len(largest_pred_list)
-
-    # print(max_num)
-    # print(largest_pred_list.count(most_predicted_obj))
+        print(pattern_id, "& {:.2f} & {:.2f} & {} & {} \\\\".format(
+            entropy([predictions.count(x) / len(predictions) for x in set(predictions)],
+                    base=2),
+            entropy([truths.count(x) / len(truths) for x in set(truths)],
+                    base=2),
+            len(set(predictions)),
+            len(set(truths))))
 
 
 if __name__ == '__main__':
