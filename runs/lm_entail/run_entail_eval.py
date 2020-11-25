69be3814495f46ce4be95f43a0a9dc774ab4bd85
Yanai Elazar <yanaiela@gmail.com>
2020-11-25 23:22:06 +0200

    bug fix in path

--- runs/lm_entail/run_entail_eval.py
+++ runs/lm_entail/run_entail_eval.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     cartesian_product = []
     for relation_id in relations:
         for lm in encoders:
-            cartesian_product.append([f'data/trex/data/TREx/{relation_id}.jsonl',
+            cartesian_product.append([f'data/trex_lms_vocab/{relation_id}.jsonl',
                                       f'data/output/predictions_lm/trex_lms_vocab/{relation_id}_{lm}.json',
                                       f'data/pattern_data/parsed/{relation_id}.jsonl',
                                       f'data/pattern_data/graphs/{relation_id}.graph'])
