725dfdabf048dc8555400a6f43c8027b0eaa7aee
Yanai Elazar <yanaiela@gmail.com>
2020-12-01 17:47:10 +0200

    update demo

--- lm_meaning/lm_entail/explore.py
+++ lm_meaning/lm_entail/explore.py
@@ -2,13 +2,18 @@ from glob import glob
 
 import pandas as pd
 import streamlit as st
+from tqdm import tqdm
 
 from lm_meaning.evaluation.entailment_probe import get_node, parse_lm_results
 from lm_meaning.utils import read_json_file, read_jsonl_file, read_graph
 
 
 def highlight_errors(row):
-    return ['background-color: #d65f5f' if not row['success'] else '' for _ in row]
+    if row['success']:
+        return ['background-color: #5fba7d'] * len(row)
+    else:
+        return ['background-color: #d65f5f'] * len(row)
+    # return ['background-color: #d65f5f' if not row['success'] else 'background-color: #5fba7d' for _ in row]
 
 
 @st.cache()
@@ -20,12 +25,31 @@ all_relations = []
 for relation in glob('data/pattern_data/graphs/*.graph'):
     all_relations.append(relation.split('/')[-1].split('.')[0])
 
+
+st.set_page_config(layout="wide")
+
+
 st.title('Exploration')
 # st.write(pattern_id)
 
-pattern_id = st.sidebar.radio('Relation', all_relations)
+possible_lms = ['bert-base-cased', 'bert-large-cased', 'bert-large-cased-whole-word-masking',
+                'robert-base', 'roberta-large',
+                'albert-base-v2', 'albert-xxlarge-v2']
+relations_file = 'data/trex/data/relations.jsonl'
+all_patterns = read_jsonl_file(relations_file)
+relations2labels = {x['relation']: x['label'] for x in all_patterns}
 
-lm = 'bert-base-cased'
+relevants_relations = [x for x in all_relations if x in relations2labels.keys()]
+relation_names = [f'{relations2labels[x]} ({x})' for x in relevants_relations]
+
+
+count = st.sidebar.slider('N. Results', min_value=10, max_value=1000, value=100)
+lm = st.sidebar.radio('LM', possible_lms, index=1)
+# pattern_id = st.sidebar.radio('Relation', all_relations)
+selected_pattern = st.sidebar.radio('Choose Relation', relation_names, index=6)
+pattern_id = selected_pattern.split(' ')[-1][1:-1]
+
+# lm = 'bert-base-cased'
 data_file = f'data/trex_lms_vocab/{pattern_id}.jsonl'
 lm_file = f'data/output/predictions_lm/trex_lms_vocab/{pattern_id}_{lm}.json'
 graph_file = f'data/pattern_data/graphs/{pattern_id}.graph'
@@ -46,7 +70,7 @@ lm_results = parse_lm_results(lm_raw_results, all_objects)
 def get_results(lm_results, patterns_graph):
     successes = []
     failures = []
-    for key, vals in lm_results.items():
+    for key, vals in tqdm(lm_results.items()):
         for successful_lm_pattern in vals:
             graph_node = get_node(patterns_graph, successful_lm_pattern)
             if graph_node is None:
@@ -68,6 +92,7 @@ def get_results(lm_results, patterns_graph):
 success_results, failure_results = get_results(lm_results, patterns_graph)
 
 df = pd.DataFrame(failure_results, columns=['base pattern', 'entailment pattern', 'subj', 'obj', 'ent-type', 'success'])
+df = df[:count]
 df_style = df.style.apply(highlight_errors, axis=1)
 st.write(df_style)
 
@@ -75,4 +100,5 @@ n = len(df)
 success_count = df['success'].value_counts()[1]
 
 st.write('overall tuples:', n)
-st.write('overall model success:', success_count, success_count / n)
+st.write('overall success:', success_count)
+st.write('% success:', success_count / n)
