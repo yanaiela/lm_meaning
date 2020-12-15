ced11bed8a7d8f19cf6cee49e4fecaba24ec03e4
Yanai Elazar <yanaiela@gmail.com>
2020-12-15 16:04:20 +0200

    auto run

---
+++ runs/evaluate/run_lm_consistent.py
@@ -0,0 +1,78 @@
+import argparse
+from runs.ts_run import parallelize
+from runs.utils import get_lama_patterns
+
+
+# ┌──────────────────────┐
+# │ connect to all nodes │
+# └──────────────────────┘
+nodes = [
+    'nlp05',
+    'nlp06',
+    'nlp09',
+    'nlp10',
+    'nlp11',
+    'nlp12',
+    'nlp13',
+    'nlp15',
+]
+
+
+# ┌──────────┐
+# │ encoders │
+# └──────────┘
+encoders = [
+            # 'bert-base-cased',
+            # 'bert-large-cased',
+            # 'bert-large-cased-whole-word-masking',
+            # 'roberta-base',
+            # 'roberta-large',
+            # 'albert-base-v2',
+            # 'albert-xxlarge-v2',
+            #
+            # 'nyu-mll/roberta-base-1B-1',
+            # 'nyu-mll/roberta-base-100M-1',
+            # 'nyu-mll/roberta-base-10M-1',
+            # 'nyu-mll/roberta-med-small-1M-1'
+
+            'entailment_bert-large-cased-whole-word-masking_100_4_0_P176-P30-P39-P127'
+            'entailment_bert-large-cased-whole-word-masking_100_4_1_P176-P30-P39-P127'
+            'entailment_bert-large-cased-whole-word-masking_100_4_2_P176-P30-P39-P127'
+            'entailment_bert-large-cased-whole-word-masking_100_4_3_P176-P30-P39-P127'
+            'entailment_bert-large-cased-whole-word-masking_100_4_4_P176-P30-P39-P127'
+            'entailment_bert-large-cased-whole-word-masking_100_4_5_P176-P30-P39-P127'
+            'entailment_bert-large-cased-whole-word-masking_100_4_6_P176-P30-P39-P127'
+            'entailment_bert-large-cased-whole-word-masking_100_4_7_P176-P30-P39-P127'
+            'entailment_bert-large-cased-whole-word-masking_100_4_8_P176-P30-P39-P127'
+            'entailment_bert-large-cased-whole-word-masking_100_4_9_P176-P30-P39-P127'
+            ]
+
+
+# ┌──────────────────────┐
+# │ generate experiments │
+# └──────────────────────┘
+
+
+if __name__ == '__main__':
+
+    parse = argparse.ArgumentParser("")
+    parse.add_argument("-dry_run", "--dry_run", type=bool, help="flag to only print commands and not execute them",
+                       default=False)
+    parse.add_argument("-patterns", "--patterns", type=str, help="patterns file",
+                       default="data/trex/data/relations.jsonl")
+    args = parse.parse_args()
+
+    relations = get_lama_patterns(args.patterns)
+
+    cartesian_product = []
+    for relation_id in relations:
+        for encoder in encoders:
+
+            cartesian_product.append([f'data/pattern_data/parsed/{relation_id}.jsonl',
+                                      f'data/trex_lms_vocab/{relation_id}.jsonl',
+                                      encoder,
+                                      f'data/pattern_data/graphs/{relation_id}.graph'
+                                      ])
+
+    parallelize(nodes, cartesian_product, '/home/nlp/lazary/workspace/thesis/lm_meaning/runs/evaluate/run_lm_consistent.sh',
+                on_gpu=True, dry_run=args.dry_run)
---
+++ runs/evaluate/run_lm_consistent.sh
@@ -0,0 +1,19 @@
+cd /home/nlp/lazary/workspace/thesis/lm_meaning/
+export PYTHONPATH=/home/nlp/lazary/workspace/thesis/lm_meaning
+
+patterns_file=$1
+data_file=$2
+lm=$3
+graph=$4
+
+
+/home/nlp/lazary/anaconda3/envs/memorization/bin/python lm_meaning/evaluation/encode_consistency_probe.py \
+        --patterns_file $patterns_file \
+        --data_file $data_file \
+        --lm $lm \
+        --graph $graph \
+        --gpu 0 \
+        --evaluate \
+        --wandb \
+        --use_targets
+
