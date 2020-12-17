18e1cd2f55bf17397edb8c2e221006db1c8ce1ac
Yanai Elazar <yanaiela@gmail.com>
2020-12-17 15:49:41 +0200

    updated the graphs creation to the new patterns differences. also, currently only keeping patterns that are entailed from the base pattern

--- lm_meaning/spike_patterns/create_graph.py
+++ lm_meaning/spike_patterns/create_graph.py
@@ -6,10 +6,11 @@ from typing import List, Tuple, Dict
 import networkx as nx
 import tqdm
 from spike.spacywrapper.annotator import SpacyAnnotator
+import spacy
 
 import wandb
 
-from lm_meaning.spike.utils import equal_queries
+from lm_meaning.spike.utils import equal_queries, lexical_difference
 from lm_meaning.spike_patterns.graph_types import PatternNode, EdgeType
 from lm_meaning.utils import read_jsonl_file
 
@@ -47,9 +48,12 @@ def load_lemmas_relations(fname: str) -> Dict[str, List[str]]:
     return lemma2not_entailed
 
 
-def get_neighbors(pattern: dict, all_patterns: List[dict], enforce_tense: bool, lemma2not_entailed: List[Tuple],
-                  connections: dict, spike_annotator) -> List[dict]:
+def get_neighbors(pattern: dict, all_patterns: List[dict], enforce_tense: bool,
+                  lemma2not_entailed: Dict[str, List[str]],
+                  connections: dict, spike_annotator,
+                  spacy_annotator) -> List[dict]:
     """
+    :param lemma2not_entailed:
     :param pattern: the pattern for which we look for neighbors in the graph (a dictionary)
     :param all_patterns: the list of all patterns
     :parma enforce_tense: if true, look for patterns having the same tense
@@ -69,12 +73,13 @@ def get_neighbors(pattern: dict, all_patterns: List[dict], enforce_tense: bool, 
     # print("{} neighbors".format(len(relevant_patterns)))
     for r_p in tqdm.tqdm(relevant_patterns, total=len(relevant_patterns)):
         # print(r_p["pattern"], pattern["pattern"])
-        if equal_queries(r_p["spike_query"], pattern["spike_query"], spike_annotator):
-            syntactic_equivalence = True
-        else:
-            syntactic_equivalence = False
-        connections[pattern["pattern"]].append((r_p["pattern"],
-                                                "same_syntax" if syntactic_equivalence else "diff_syntax"))
+        syntactic_equivalence = equal_queries(r_p["spike_query"], pattern["spike_query"], spike_annotator)
+        lexical_results = lexical_difference(r_p['pattern'], pattern['pattern'], spacy_annotator)
+        patterns_diff = {'diff_lemma': lexical_results['diff_lemma'],
+                         'diff_det': lexical_results['diff_det'],
+                         'diff_syntax': not syntactic_equivalence
+                         }
+        connections[pattern["pattern"]].append((r_p["pattern"], patterns_diff))
 
     return relevant_patterns
 
@@ -83,51 +88,55 @@ if __name__ == "__main__":
 
     parse = argparse.ArgumentParser("")
     parse.add_argument("-patterns_file", "--patterns_file", type=str, help="pattern file",
-                       default="data/output/P449.jsonl")
+                       default="data/pattern_data/parsed/P449.jsonl")
     parse.add_argument("-lemmas_file", "--lemmas_file", type=str, help="lemmas file",
-                       default="data/output/P449_entailment_lemmas.tsv")
+                       default="data/pattern_data/entailed_lemmas_extended/P449.tsv")
     parse.add_argument("-out_file", "--out_file", type=str, help="output file",
                        default="data/pattern_data/graphs/P449.graph")
 
     args = parse.parse_args()
     log_wandb(args)
+    entailed_from_base = True
+
+    nlp = spacy.load('en_core_web_sm')
 
     patterns = read_jsonl_file(args.patterns_file)
     lemma2not_entailed = load_lemmas_relations(args.lemmas_file)
     graph = nx.DiGraph()
     pattern2node = dict()
 
+    # collection connections dictionary
+
+    print("Calculating connections between nodes...")
+    spike_annotator = SpacyAnnotator.from_config("en.json")
+
+    connections = defaultdict(list)
+    for pattern in tqdm.tqdm(patterns, total=len(patterns)):
+        get_neighbors(pattern, patterns, False, lemma2not_entailed, connections, spike_annotator, nlp)
+
+    base_pattern = patterns[0]
+    entailed_patterns = [x for x in patterns if x['pattern'] in [t[0] for t in connections[base_pattern['pattern']]]]
+    if entailed_from_base:
+        subset_patterns = [base_pattern] + entailed_patterns
+    else:
+        subset_patterns = patterns
     print("Creating graph...")
-    for p in tqdm.tqdm(patterns, total=len(patterns)):
+    for p in tqdm.tqdm(subset_patterns, total=len(patterns)):
         pattern_node = PatternNode(p["pattern"], p["spike_query"],
                                    p["lemma"], p["extended_lemma"], p["tense"], p["example"])
         pattern2node[p["pattern"]] = pattern_node
         graph.add_node(pattern_node)
 
-    # collection connections dictionary
-
-    print("Calcualting connections between nodes...")
-    spike_annotator = SpacyAnnotator.from_config("en.json")
-
-    connections = defaultdict(list)
-    for pattern in tqdm.tqdm(patterns, total=len(patterns)):
-        get_neighbors(pattern, patterns, False, lemma2not_entailed, connections, spike_annotator)
-
     # fill the graph with that info
     print("Filling the graph...")
-    for i, pattern in enumerate(tqdm.tqdm(patterns, total=len(patterns))):
-        print("Pattern {}/{}".format(i, len(patterns)))
+    for i, pattern in enumerate(tqdm.tqdm(subset_patterns, total=len(subset_patterns))):
+        print("Pattern {}/{}".format(i, len(subset_patterns)))
         pattern_str = pattern["pattern"]
         connected_patterns, types = zip(*connections[pattern_str])
         for pattern_str2, typ in zip(connected_patterns, types):
             node1, node2 = pattern2node[pattern_str], pattern2node[pattern_str2]
-            different_lemma = node1.extended_lemma != node2.extended_lemma
-            if typ == "diff_syntax" and different_lemma:  # different lemma, different syntax
-                edge_type = EdgeType.both
-            elif typ == "diff_syntax":  # different syntax, same lemma
-                edge_type = EdgeType.syntactic
-            else:  # same syntax, different lemma
-                edge_type = EdgeType.lexical
+            # different_lemma = node1.extended_lemma != node2.extended_lemma
+            edge_type = EdgeType(typ['diff_syntax'], typ['diff_lemma'], typ['diff_det'])
 
             graph.add_edge(node1, node2, edge_type=edge_type)
 
