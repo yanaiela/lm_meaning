a6c1c0137b97f1ebbecb8b4770144220e24c4408
Nora Kassner <nora.kassner@campus.lmu.de>
2020-12-23 23:35:03 +0100

    data global consistancy

--- data/enailment_train/generate_data_consistancy_global.py
+++ data/enailment_train/generate_data_consistancy_global.py
@@ -14,10 +14,10 @@ from lm_meaning import utils
 
 def generate_data(num_relations, num_tuples, LAMA_path):
 
-
-    graph_path = "data/pattern_data/graphs/"
+    num_relations -= 1
+    graph_path = "data/pattern_data/graphs_sub/"
     relations_path =  glob.glob(graph_path + "*.graph")
-    output_path = "data/enailment_train/consistancy_relation_"
+    output_path = "data/enailment_train/global_consistancy_relation_"
 
     output_path = output_path + str(num_relations+1) + "/"
 
@@ -28,74 +28,65 @@ def generate_data(num_relations, num_tuples, LAMA_path):
     output_path = output_path + timestr + "/"
     os.mkdir(output_path)
 
-    output_path_true =  output_path + "train.txt"
+    output_path_true =  output_path + "train_consistancy_"
+    output_path_mlm =  output_path + "train_mlm.txt"
     output_path_log = output_path + "log.txt"
 
-    f_true = open(output_path_true, "w")
     f_log = open(output_path_log, "w")
-
-    all_patterns = {}
-    for relation_path in relations_path:
-        if "temp" in relation_path:
-            continue
-        with open(relation_path, "rb") as f:
-             graph = pickle.load(f)
-        if len(graph.nodes())==0:
-            continue
-        relation = relation_path.split("/")[-1].split(".")[0]
-        all_patterns[relation] = {}
-        all_patterns[relation]["patterns"] = []
-        for node in graph.nodes():
-             all_patterns[relation]["patterns"].append(node.lm_pattern)
-
-        all_patterns[relation]["sub_obj"] = []
-        data = utils.read_jsonl_file(LAMA_path + relation + ".jsonl")
-        for d in data:
-            all_patterns[relation]["sub_obj"].append([d["sub_label"], d["obj_label"]])
+    f_mlm = open(output_path_mlm, "w")
 
     random.shuffle(relations_path)
+
 
     f_log.write(str(num_tuples))
     f_log.write("\n")
 
     for i_relations, relation_path in enumerate(relations_path):
+
         with open(relation_path, "rb") as f:
             graph = pickle.load(f)
         relation = relation_path.split("/")[-1].split(".")[0]
+
+        
+        f_true = open(output_path_true + relation + ".txt", "w")
+        #f_mlm = open(output_path_mlm + relation + ".txt", "w")
+
 
         f_log.write(relation)
         f_log.write("\n")
         data = utils.read_jsonl_file(LAMA_path + relation + ".jsonl")
         random.shuffle(data)
+
         for i, d in enumerate(data):
             random.shuffle(data)
             for node in graph.nodes():
                 pattern = node.lm_pattern
-                #premise = edge[0].lm_pattern
-                #hypothesis = edge[1].lm_pattern
 
                 pattern = pattern.replace("[X]", d["sub_label"])
-                pattern = pattern.replace("[Y]", d["obj_label"])
+                pattern = pattern.replace("[Y]", "[MASK]")
+                pattern_mlm = pattern.replace("[MASK]", d["obj_label"])
 
-                #hypothesis = hypothesis.replace("[X]", d["sub_label"])
-                #hypothesis = hypothesis.replace("[Y]", d["obj_label"])
-                if i < num_tuples:
-                    f_true.write(pattern)
-                    f_true.write("\n")
+                f_true.write(pattern)
+                f_true.write("\n")
+                f_mlm.write(pattern_mlm)
+                f_mlm.write("\n")
 
-                else:
-                    continue
+
             f_true.write("\n")
+
+            if i >= num_tuples:
+                break
+        f_true.close()
+        #f_mlm.close()
         if i_relations==num_relations:
             break
-
-    f_true.close()
+    f_mlm.close()
     f_log.close()
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_relations', '-nr', type=int, default=5, help='number of relations')
-    parser.add_argument('--num_tuples', '-nt', type=int, default=100, help='number of tuples')
+    parser.add_argument('--num_tuples', '-nt', type=int, default=2000, help='number of tuples')
     parser.add_argument('--LAMA_path', '-lama', type=str, default="/mounts/data/proj/kassner/LAMA/data/TREx/", help='number of tuples')
 
 
