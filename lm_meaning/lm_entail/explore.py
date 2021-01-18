be5cf7232a212fbcb20efc7639f6e4a97bc0d8e4
Yanai Elazar <yanaiela@gmail.com>
2021-01-18 13:24:24 +0200

    demo update

--- lm_meaning/lm_entail/explore.py
+++ lm_meaning/lm_entail/explore.py
@@ -2,18 +2,15 @@ from glob import glob
 
 import pandas as pd
 import streamlit as st
-from tqdm import tqdm
 
-from lm_meaning.evaluation.entailment_probe import get_node, parse_lm_results
 from lm_meaning.utils import read_json_file, read_jsonl_file, read_graph
 
 
 def highlight_errors(row):
-    if row['success']:
+    if row['pred1'] == row['pred2']:
         return ['background-color: #5fba7d'] * len(row)
     else:
         return ['background-color: #d65f5f'] * len(row)
-    # return ['background-color: #d65f5f' if not row['success'] else 'background-color: #5fba7d' for _ in row]
 
 
 @st.cache()
@@ -52,9 +49,9 @@ pattern_id = selected_pattern.split(' ')[-1][1:-1]
 # lm = 'bert-base-cased'
 data_file = f'data/trex_lms_vocab/{pattern_id}.jsonl'
 lm_file = f'data/output/predictions_lm/trex_lms_vocab/{pattern_id}_{lm}.json'
-graph_file = f'data/pattern_data/graphs/{pattern_id}.graph'
+graph_file = f'data/pattern_data/graphs_tense/{pattern_id}.graph'
 
-lm_raw_results = read_json_file(lm_file)
+lm_results = read_json_file(lm_file)
 patterns_graph = read_graph(graph_file)
 
 data = get_data(data_file)
@@ -64,41 +61,38 @@ for row in data:
 
 all_objects = list(set(subj_obj.values()))
 
-lm_results = parse_lm_results(lm_raw_results, all_objects)
-
 
 def get_results(lm_results, patterns_graph):
     successes = []
     failures = []
-    for key, vals in tqdm(lm_results.items()):
-        for successful_lm_pattern in vals:
-            graph_node = get_node(patterns_graph, successful_lm_pattern)
-            if graph_node is None:
-                continue
+    patterns = list(lm_results.keys())
+    keys = list(lm_results[patterns[0]].keys())
+    for i in range(len(patterns)):
+        for j in range(i + 1, len(patterns)):
+            for k in keys:
 
-            # going over all entailed patterns
-            for ent_node in patterns_graph.successors(graph_node):
-                if [graph_node, ent_node] not in patterns_graph.edges:
-                    continue
-                entailment_type = patterns_graph.edges[graph_node, ent_node]['edge_type'].name
+                failures.append([
+                    patterns[i],
+                    patterns[j],
+                    k,
+                    lm_results[patterns[i]][k][1],
+                    lm_results[patterns[i]][k][0],
+                    lm_results[patterns[j]][k][0]
+                ])
 
-                ent_pattern = ent_node.lm_pattern
-                success = ent_pattern in lm_results[key]
-                failures.append(
-                    [graph_node.lm_pattern, ent_node.lm_pattern, ] + key.split('_SPLIT_') + [entailment_type, success])
     return successes, failures
 
 
 success_results, failure_results = get_results(lm_results, patterns_graph)
 
-df = pd.DataFrame(failure_results, columns=['base pattern', 'entailment pattern', 'subj', 'obj', 'ent-type', 'success'])
+df = pd.DataFrame(failure_results, columns=['pattern1', 'pattern2', 'subj', 'obj', 'pred1', 'pred2'])
 df = df[:count]
 df_style = df.style.apply(highlight_errors, axis=1)
 st.write(df_style)
 
 n = len(df)
-success_count = df['success'].value_counts()[1]
+# success_count = df['success'].value_counts()[1]
 
 st.write('overall tuples:', n)
-st.write('overall success:', success_count)
-st.write('% success:', success_count / n)
+# st.write('overall success:', success_count)
+# st.write('% success:', success_count / n)
